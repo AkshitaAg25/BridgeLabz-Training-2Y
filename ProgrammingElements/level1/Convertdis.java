dc7cacc7850df792a433d5e345069fbb248d1bff
AkshitaAg25 <akshitaa160@gmail.com>
2025-08-21 11:17:57 +0530

    [Akshita] added file

--- ProgrammingElements/level1/Convertdis.java
+++ ProgrammingElements/level1/Convertdis.java
@@ -5,10 +5,8 @@ public class Convertdis {
         double km=10.8;
         final double CONVERSION_FACTOR = 0.621371;
 
-        // Calculate the distance in miles
         double miles = km * CONVERSION_FACTOR;
 
-        // Print the result
         System.out.println(km + " kilometers is equal to " + miles + " miles.");
     }
  }
