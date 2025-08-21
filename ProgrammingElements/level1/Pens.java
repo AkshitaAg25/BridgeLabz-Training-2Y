f8940bcca08a79f0f72fc35bd99995de7fa8d0c6
AkshitaAg25 <akshitaa160@gmail.com>
2025-08-21 11:57:03 +0530

    [Akshita] added pens file

---
+++ ProgrammingElements/level1/Pens.java
@@ -0,0 +1,11 @@
+public class Pens {
+    public static void main(String[] args) {
+        int totalpens= 14;
+        int students= 3;
+        int eachget=totalpens/students;
+        int remaining =totalpens%students;
+        System.out.println("The Pen Per Student is "+eachget+" and the remaining pen not distributed is "+remaining);
+    }
+    
+
+}
