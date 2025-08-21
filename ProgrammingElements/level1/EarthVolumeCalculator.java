d5a820991ce2bec24bafe62313542c7dbb60b367
AkshitaAg25 <akshitaa160@gmail.com>
2025-08-21 12:07:15 +0530

    [Akshita] added earth file

---
+++ ProgrammingElements/level1/EarthVolumeCalculator.java
@@ -0,0 +1,16 @@
+public class EarthVolumeCalculator {
+
+    public static void main(String[] args) {
+        double radiusKm = 6378.0;
+        double pi = Math.PI;
+
+        double volumeKm3 = (4.0 / 3.0) * pi * Math.pow(radiusKm, 3);
+        
+        // Conversion factor from km to miles: 1 km = 0.621371 miles
+        double radiusMiles = radiusKm * 0.621371;
+
+        double volumeMiles3 = (4.0 / 3.0) * pi * Math.pow(radiusMiles, 3);
+
+        System.out.printf("The volume of earth in cubic kilometers is %.2f and cubic miles is %.2f%n", volumeKm3, volumeMiles3);
+    }
+}
